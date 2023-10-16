188d4cd98f34ed3d604cbab4e7f12383fec7aa2f
Sebastian Martinez <sgmartinezlp@gmail.com>
2023-10-16 15:28:38 +0800

    Improved accessibility and visiblity

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,18 @@ module.exports = {
     require("daisyui")
   ],
   daisyui: {
-    themes: ["corporate"]
+    themes: [
+      {
+        "corporate-custom": {
+          ...require("daisyui/src/theming/themes")["[data-theme=corporate]"],
+          "primary": "#2b44b6",
+          "primary-focus": "#1e43e6",
+          "error": "#ef4444",
+          "base-200": "#ededed",
+          "base-300": "blue"
+        }
+      }
+    ]
   }
 }
 
